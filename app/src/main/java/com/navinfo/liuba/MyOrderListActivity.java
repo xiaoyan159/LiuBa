4dcc650411520c6691db6523f0e34c789950abf9
xiaoyan159 <xiaoyan159@163.com>
2017-09-14 15:20:50 +0800

    添加viewpager

--- app/src/main/java/com/navinfo/liuba/MyOrderListActivity.java
+++ app/src/main/java/com/navinfo/liuba/MyOrderListActivity.java
@@ -1,15 +1,33 @@
 package com.navinfo.liuba;
 
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+
+import com.shizhefei.view.indicator.Indicator;
+import com.shizhefei.view.indicator.IndicatorViewPager;
 
 /**
  * Created by zhangdezhi1702 on 2017/9/14.
  */
 
 public class MyOrderListActivity extends BaseActivity{
+    private IndicatorViewPager indicatorViewPager;
+    private LayoutInflater inflate;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order_list);
+
+        ViewPager viewPager = (ViewPager) findViewById(R.id.order_type_viewPager);
+        Indicator indicator = (Indicator) findViewById(R.id.order_type_indicator);
+        // 将viewPager和indicator使用
+        indicatorViewPager = new IndicatorViewPager(indicator, viewPager);
+        inflate = LayoutInflater.from(getApplicationContext());
+        // 设置indicatorViewPager的适配器
+//        indicatorViewPager.setAdapter(adapter);
     }
+
+
 }
