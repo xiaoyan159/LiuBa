dbc4e4b7d47c9d6726ca4c0b82d71846869de93d
xiaoyan159 <xiaoyan159@163.com>
2017-09-15 14:37:24 +0800

    添加部分图标，修改注册界面和主界面

--- app/src/main/java/com/navinfo/liuba/MainActivity.java
+++ app/src/main/java/com/navinfo/liuba/MainActivity.java
@@ -3,15 +3,14 @@ package com.navinfo.liuba;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
 
 public class MainActivity extends BaseActivity implements View.OnClickListener {
 
     private ImageView mImgUserCenter;
     private ImageView mImgGps;
-    private Button btnWalk;
-    private Button btnAppoint;
+    private View btnWalk;
+    private View btnAppoint;
     private View mView;
 
     @Override
@@ -21,8 +20,8 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
         setContentView(mView);
         mImgUserCenter = (ImageView) mView.findViewById(R.id.user_center);
         mImgGps = (ImageView) mView.findViewById(R.id.gps_location);
-        btnWalk = (Button) mView.findViewById(R.id.walk_the_dog);
-        btnAppoint = (Button) mView.findViewById(R.id.make_an_appointment);
+        btnWalk = mView.findViewById(R.id.walk_the_dog);
+        btnAppoint = mView.findViewById(R.id.make_an_appointment);
         mImgUserCenter.setOnClickListener(this);
         mImgGps.setOnClickListener(this);
         btnWalk.setOnClickListener(this);
@@ -38,10 +37,11 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
 
                 break;
             case R.id.walk_the_dog:
-                Intent intent = new Intent(MainActivity.this, OrderInputActivity.class);
-                startActivity(intent);
+
                 break;
             case R.id.make_an_appointment:
+                Intent intent = new Intent(MainActivity.this, OrderInputActivity.class);
+                startActivity(intent);
                 break;
         }
     }
